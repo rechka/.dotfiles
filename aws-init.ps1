9ae7ec9cde023c4609dbbd421b594ea2aa512163
rechka <chto@zachem.ru>
2021-03-23 15:31:34 -0400

    ie sec test

--- aws-init.ps1
+++ aws-init.ps1
@@ -9,4 +9,18 @@ choco install notepadplusplus.install -y
 
 (new-object net.webclient).DownloadFile('https://github.com/xrzes/scottbotv1-releases/releases/download/v2.6.17/scottbotv1-Setup-2.6.17.exe','c:\scottbotv1-Setup-2.6.17.exe')
 
+function Disable-ieESC
+
+{
+	$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+	$UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+	Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+	Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+	Stop-Process -Name Explorer
+	Write-Host “IE Enhanced Security Configuration (ESC) has been disabled.” -ForegroundColor Green
+}
+
+Disable-ieESC
+
+
 </powershell>
