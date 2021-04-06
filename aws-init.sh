34c0cae98dbd5f1426c2786fdfebd9c7be5b8c0e
rechka <rechka@users.noreply.github.com>
2021-04-06 13:12:46 -0400

    Update aws-init.sh

--- aws-init.sh
+++ aws-init.sh
@@ -114,6 +114,8 @@ rm -rf /var/lib/cloud/instances/i-*/scripts/
 rm -f /var/lib/cloud/instances/i-*/user-data.txt*
 
 pip3 install jupyterlab==3
+pip3 install "jupyterlab-kite>=2.0.2"
+pip3 install tabula requests-html 
 
 export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
