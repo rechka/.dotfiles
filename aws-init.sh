394d866b4dc76dbba9bde777a97be2edcdee779a
rechka <rechka@users.noreply.github.com>
2021-04-06 13:40:46 -0400

    Update aws-init.sh

--- aws-init.sh
+++ aws-init.sh
@@ -118,6 +118,9 @@ pip3 install "jupyterlab-kite>=2.0.2"
 pip3 install tabula requests-html 
 apt-get -y install jupyter-core
 
+su - -c "echo -e \"$KITE_PASS\" | ~/.local/share/kite/login-user \"$KITE_LOGIN\"" $username
+
+
 export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 
