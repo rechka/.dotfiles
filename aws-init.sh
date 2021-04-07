90b922aca2637e8a9d5be5329b91b8f2c7ba5e6b
rechka <rechka@users.noreply.github.com>
2021-04-07 00:49:44 -0400

    Update aws-init.sh

--- aws-init.sh
+++ aws-init.sh
@@ -85,7 +85,7 @@ su - -c 'curl -L https://zulu.molovo.co/install | zsh' $username
 #dotfiles, watch out for variable in repo url
 su - -c "git clone https://github.com/$username/.dotfiles.git && rcup -f rcrc && rcup -f" $username
 # add to PATH /home/$username/.local/bin
-su - -c 'pip3 -q install -r ~/.dotfiles/requirements.txt --no-warn-script-location' $username
+su - -c 'pip3 -q install --no-warn-script-location -r ~/.dotfiles/requirements.txt' $username
 
 su - -c 'source ~/.zulu/core/zulu && zulu init && \
 zulu install async fast-syntax-highlighting solarized-man z \
