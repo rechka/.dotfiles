b178b1f09ff0efdfdca694dabde42f452028a3ad
rechka <rechka@users.noreply.github.com>
2021-04-08 09:23:39 -0400

    fix typo

--- aws-init.sh
+++ aws-init.sh
@@ -121,7 +121,7 @@ ln -s /snap/bin/certbot /usr/bin/certbot
 certbot certonly --standalone -m $EMAIL --agree-tos --domains $DOMAIN -n
 setfacl -R -m u:$username:rX /etc/letsencrypt/{live,archive}/$DOMAIN
 setfacl -m u:$username:rX /etc/letsencrypt/{live,archive}
-su - -c 'ln -s /etc/letsencrypt/live/'"'$DOMAIN'"'/fullchain.pem /home/'"'$username'"'/.jupyter/.fullchain' $username
+su - -c 'ln -s /etc/letsencrypt/live/'"'$DOMAIN'"'/fullchain.pem /home/'"'$username'"'/.jupyter/.cert' $username
 su - -c 'ln -s /etc/letsencrypt/live/'"'$DOMAIN'"'/privkey.pem /home/'"'$username'"'/.jupyter/.key' $username
 
 #source https://gist.github.com/danibram/d00ed812f2ca6a68758e
