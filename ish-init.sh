3e0578ba8d621b1f5fca128cfd843ca372a1b21f
rechka <rechka@users.noreply.github.com>
2021-04-24 23:15:21 -0400

    v2

--- ish-init.sh
+++ ish-init.sh
@@ -9,48 +9,34 @@ apk del tzdata
 
 apk -U upgrade
 apk add git zsh gawk gpgme curl bash ripgrep nodejs npm openssh \
- make go ranger aws-cli jq rcm xclip rclone byobu
+ make ranger aws-cli jq rcm rclone fzf sudo
+
+mount -t ios whatever /mnt
+gpg --import /mnt/$username.asc
+umount -t ios /mnt
 
 #snaps used to be here
-curl https://getmic.ro | bash && mv micro /usr/bin
+curl https://getmic.ro | bash && mv micro /bin
 git clone --depth 1 https://github.com/sobolevn/git-secret.git
 cd git-secret && make build && PREFIX="/usr/local" make install
+cd ~
+git clone --depth 5 https://github.com/$username/.dotfiles.git && rcup -f rcrc
+cd .dotfiles && git secret reveal && rcup -f
 
 # 1.13.15 + 2 errors
 #curl -fLo /tmp/go1.16.3.linux-amd64.tar.gz https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
 #rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.16.3.linux-amd64.tar.gz
 #rm /tmp/go1.16.3.linux-amd64.tar.gz
 
-glances suckless fzf
+#suckless
 
 #user
-adduser --gecos "" --disabled-password --ingroup adm --shell /usr/bin/zsh --debug --add_extra_groups $username
+adduser -g "" -D -G adm -s /bin/zsh $username
 passwd -d $username
-usermod -aG sudo $username
-#addgroup --system docker
-#usermod -aG docker $username
-#service docker restart
-
-cp -r ~/.ssh /home/$username/
-chown $username.adm -R /home/$username/.ssh
-chmod 700 /home/$username/.ssh
-
-#chrome 84
-#cd /tmp
-#wget -q https://launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/19746659/+files/chromium-browser_84.0.4147.105-0ubuntu0.16.04.1_amd64.deb
-#wget -q https://launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/19746659/+files/chromium-codecs-ffmpeg_84.0.4147.105-0ubuntu0.16.04.1_amd64.deb
-#dpkg -i -E chromium-*.deb
-#apt-mark hold chromium-browser
-#apt-mark hold chromium-codecs-ffmpeg
-#rm chromium-*.deb
-#cd /root
+#?usermod -aG sudo $username
 
 #zulu 
 su - -c 'curl -L https://zulu.molovo.co/install | zsh' $username
-
-#dotfiles, watch out for variable in repo url
-su - -c "git clone https://github.com/$username/.dotfiles.git && rcup -f rcrc && rcup -f" $username
-#su - -c 'pip3 install -U --no-warn-script-location -r ~/.dotfiles/requirements.txt' $username
 
 su - -c 'source ~/.zulu/core/zulu && zulu init && \
 zulu install async fast-syntax-highlighting z \
@@ -73,58 +59,12 @@ cp /home/$username/.ssh/known_hosts ~/.ssh/
 #ssh -vT git@github.com
 su - -c "cd ~/.dotfiles && git remote set-url origin git@github.com:$username/.dotfiles.git" $username
 
-
-#lab
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install jupyterlab-topbar-text --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install jupyterlab-topbar-extension --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install jupyterlab-theme-toggle --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install jupyterlab-spreadsheet --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install @jupyterlab/debugger --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension uninstall nbdime-jupyterlab --no-build' $username
-#su - -c 'PATH=~/.local/bin:$PATH jupyter contrib nbextension install --user' $username
-#jupyter labextension install @datalayer-jupyter/jupyterlab-git
-#jupyter labextension install @jupyterlab/shortcutui
-#jupyter labextension install @oriolmirosa/jupyterlab_materialdarker
-#su - -c 'PATH=~/.local/bin:$PATH jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build' $username
-#jupyter labextension install @jupyterlab/google-drive
-#jupyter labextension install jupyterlab-flake8
-#su - -c 'PATH=~/.local/bin:$PATH jupyter lab build' $username
-
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
-#certbot certonly --standalone -m $EMAIL --agree-tos --domains $DOMAIN -n
-git clone git@github.com:rechka/lec.git /etc/letsencrypt
-certbot certificates
-setfacl -R -m u:$username:rX /etc/letsencrypt/{live,archive}/$DOMAIN
-setfacl -m u:$username:rX /etc/letsencrypt/{live,archive}
-su - -c 'ln -s /etc/letsencrypt/live/'"'$DOMAIN'"'/fullchain.pem /home/'"'$username'"'/.jupyter/.cert' $username
-su - -c 'ln -s /etc/letsencrypt/live/'"'$DOMAIN'"'/privkey.pem /home/'"'$username'"'/.jupyter/.key' $username
-
-#source https://gist.github.com/danibram/d00ed812f2ca6a68758e
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
-iptables -t nat -A OUTPUT -o lo -p tcp --dport 443 -j REDIRECT --to-port 8443
-sh -c "iptables-save > /etc/iptables.rules"
-
-#source https://gist.github.com/alonisser/a2c19f5362c2091ac1e7
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
-apt-get -y install iptables-persistent
-
 # push etckeeper
 #sed -i "s/PUSH_REMOTE=\"\"/PUSH_REMOTE=\"origin\"/g" /etc/etckeeper/etckeeper.conf
 #cd /etc && git add . && git commit -m "userdata complete" && git push -u origin `git branch --show-current`
-
-#remove myself
-rm -rf /var/lib/cloud/instances/i-*/scripts/
-rm -f /var/lib/cloud/instances/i-*/user-data.txt*
-
-
-
 
 #inform about readiness
 curl -X POST \
   -H "Content-Type: application/json" \
   -d '{"username": "'"$HOSTNAME"'", "content": "'"I live in $PUBLIC_IPV4"'"}' \
-  $WEBHOOK_URL
-  
+  $WEBHOOK_URL 
