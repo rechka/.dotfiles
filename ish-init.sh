c1a634438894953646a2cdd5862b9703871cacaa
rechka <rechka@users.noreply.github.com>
2021-04-28 17:38:08 -0400

    Update ish-init.sh

--- ish-init.sh
+++ ish-init.sh
@@ -3,7 +3,7 @@ echo https://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositor
 echo "https://dl-cdn.alpinelinux.org/alpine/v3.13/community" >> /etc/apk/repositories
 echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk upgrade --update-cache --available
-for pkg in git zsh tzdata curl openssh ncurses tmux screen byobu \
+for pkg in git zsh tzdata curl openssh ncurses tmux screen \
  ranger jq rcm gawk sudo neovim git-secret micro ; do
   echo ** adding $pkg
   apk add $pkg
