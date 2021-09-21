96656b908e85494b1b634a76f1dad256f8fc5126
rechka <rechka@users.noreply.github.com>
2021-09-20 21:41:31 -0400

    Update brew.zsh

--- brew.zsh
+++ brew.zsh
@@ -1,15 +1,16 @@
 brew tap mvp/uhubctl https://github.com/mvp/uhubctl
+brew tap facebook/fb
 brew install libusb uhubctl 
 
 brew install bat bat-extras cmake coreutils flake8 \
-	fzf gawk git git-secret gitignore gnirehtet howdoi htop icdiff jq legit \
-	mas micro pipx poetry prettier pssh pyenv python@3.9 qemu qpdf ranger \
-  rethinkdb ripgrep rsync scrcpy serverless shopify-cli terraform \
-  the_silver_searcher tmux tmux-xpanes wget zeromq 
+    fzf gawk git git-secret gitignore gnirehtet howdoi htop icdiff jq legit \
+    mas micro pipx poetry prettier pssh pyenv python@3.9 qemu qpdf ranger \
+    rethinkdb ripgrep rsync scrcpy serverless shopify-cli terraform \
+    the_silver_searcher tmux tmux-xpanes wget zeromq 
   
 brew install airtable android-platform-tools android-sdk anydesk bluestacks \
-	clipy cocoarestclient cyberduck deezer discord dropbox firefox \
-	font-fira-code-nerd-font google-chrome http-toolkit iterm2 \
-	keepingyouawake keka nordvpn raspberry-pi-imager rectangle signal slack \
-	sonos sublime-text sync telegram ubersicht visual-studio-code vlc \
-	whatsapp xcodes zerotier-one zoom
+    clipy cocoarestclient cyberduck deezer discord dropbox firefox \
+    font-fira-code-nerd-font google-chrome http-toolkit iterm2 \
+    keepingyouawake keka nordvpn raspberry-pi-imager rectangle signal slack \
+    sonos sublime-text sync telegram ubersicht visual-studio-code vlc \
+    whatsapp xcodes zerotier-one zoom
